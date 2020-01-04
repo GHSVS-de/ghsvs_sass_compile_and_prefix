b3dc56d71c8dbe37a39d6bb7697217c0cb2707c9
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2020-01-04 03:34:24 +0100

    Blah

--- bin/help.php
+++ bin/help.php
@@ -16,8 +16,6 @@ else
 	$output[] = '### Output of `npm run ghs-help` (path variables not translated)';
 }
 
-#$output[] = '#### package.json. Example `DIR` config.' . NL . ' ```' . print_r($packageJson['DIR'], true) . '```' . NL;
-
 $scriptsHelp = array(
 	'What?' => 'Compiles, minifies, autoprefixes *.scss files to CSS and creates source-maps.',
 	'Tested?' => 'Only with WSL (Windows Subsystem for Linux) + PHP 7.3 + Node + NPM on local machine.', 
@@ -27,7 +25,7 @@ $scriptsHelp = array(
 	. '- target: Variable `$npm_package_DIR_target` (absolute path). Dir (normally a template folder) where all the compiled, prefixed, minified CSS files (inside subfolders) are copied to after all these steps are finished.' . NL
 	. '- work: Variable `$npm_package_DIR_work` (relative path). Temporary local work directory inside same folder where package.json is located.' . NL
 	. '- css: Variable `$npm_package_DIR_css` (relative path. Dir where all compiled, prefixed, minified CSS files are copied to.' . NL
-	. '- raw:  Variable $npm_package_DIR_raw (relative path). Dir where all compiled, minified **BUT NOT PREFIXED** CSS files are copied to.' . NL
+	. '- raw:  Variable `$npm_package_DIR_raw` (relative path). Dir where all compiled, minified **BUT NOT PREFIXED** CSS files are copied to.' . NL
 	. '- ftp:  Variable `$npm_package_DIR_ftp` (relative path). Dir where all compiled, prefixed, minified CSS files are copied to. Source folder for FTP transfer.' .NL
 	. NL . '##### package.json. Example `DIR` config.' . NL . ' ```' . print_r($packageJson['DIR'], true) . '```'
 );
